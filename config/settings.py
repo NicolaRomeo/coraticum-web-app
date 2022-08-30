d3dbfce90db90ebab5b51329da313ce5a4f1c33b
nromeo <nicolaromeo1@gmail.com>
2022-08-30 14:52:37 +0200

    cors http

--- config/settings.py
+++ config/settings.py
@@ -111,9 +111,8 @@ AUTH_PASSWORD_VALIDATORS = [
 #CORS permissions
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://127.0.0.1:8000/",
-    'coraticum-web.herokuapp.com',
-
+    "http://127.0.0.1:8000",
+    'http://coraticum-web.herokuapp.com',
 ]
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
