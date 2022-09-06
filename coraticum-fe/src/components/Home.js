f63be286c12896185e515dd46c1725ebb2fe8a0e
nromeo <nicolaromeo1@gmail.com>
2022-09-06 17:53:36 +0200

    trying with 0000

--- coraticum-fe/src/components/Home.js
+++ coraticum-fe/src/components/Home.js
@@ -27,7 +27,7 @@ state= {showForm: false}
 
 showForm = () => {
    return (
-    <form action="http://localhost:9090/create-checkout-session" method="POST">
+    <form action="http://0.0.0.0:9090/create-checkout-session" method="POST">
       <button type="submit" >Pre-Order for 18,90€</button>
     </form>
      );
